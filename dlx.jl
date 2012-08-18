b01660ba6233739861c1f5a60e2d8b3f7291bdbd
Aaron Pribadi <aaron.pribadi@gmail.com>
2012-08-18 02:13:22 -0700

    Working, with test case.

--- dlx.jl
+++ dlx.jl
@@ -3,11 +3,12 @@ module DLX
 
     import Base.*
 
-    abstract Direction
-    abstract Left  <: Direction
-    abstract Right <: Direction
-    abstract Up    <: Direction
-    abstract Down  <: Direction
+    export test
+
+    abstract Left
+    abstract Right
+    abstract Up
+    abstract Down
 
     next(::Type{Left},  it) = it.left
     next(::Type{Right}, it) = it.right
@@ -19,33 +20,50 @@ module DLX
         sentinel
     end
 
-    start{T}(iter::Linked{T}) = next(T, iter.head)
+    start{T}(iter::Linked{T}) = iter.first
     done{T}(iter::Linked{T}, state) = (state == iter.sentinel)
     next{T}(iter::Linked{T}, state) = (state, next(T, state))
 
-    abstract WrappedIterator
+    function circular(T, head)
+        Linked{T}(next(T, head), head)
+    end
 
-    type Circular{T} <: WrappedIterator
-        wrapped
-        function Circular{T}(head)
-            new(Linked{T}(next(T, head), head))
+    abstract ColumnHead
+    abstract RowHead
+
+    type Root <: RowHead
+        left
+        right
+        function Root()
+            self = new()
+            self.left = self.right = self
+            self
         end
     end
 
-    start(iter::WrappedIterator) = start(iter.wrapped)
-    done(iter::WrappedIterator, state) = done(iter.wrapped, state)
-    next(iter::WrappedIterator, state) = next(iter.wrapped, state)
-
-    type ColumnHead 
+    type Column <: ColumnHead
         right
         left
         up
         down
         size
-        function ColumnHead()
+        ci
+        function Column(ci)
             self = new()
+            self.ci = ci
             self.size = 0
             self.left = self.right = self.up = self.down = self
+            self
+        end
+    end
+
+    type Row
+        head
+        ri
+        function Row(ri)
+            self = new()
+            self.ri = ri
+            self.head = nothing
             self
         end
     end
@@ -54,30 +72,32 @@ module DLX
         head.up.down = it
         it.up = head.up
         it.down = head
-        head.up = it # must be last
-        head.size += 1
+        head.up = it      # modify this last
     end
 
-    type RowHead 
-        left
-        right
-        function RowHead()
-            self = new()
-            self.left = self.right = self
-            self
-        end
+    function push(head::Column, it)
+        invoke(push, (ColumnHead, Any), head, it)
+        head.size += 1
     end
 
     function push(head::RowHead, it) 
         head.left.right = it
         it.left = head.left
         it.right = head
-        head.left = it # must be last
+        head.left = it     # modify this last
     end
 
-    type Node
-        right
+    function push(row::Row, it)
+        if row.head == nothing
+            row.head = it
+        else
+            invoke(push, (RowHead, Any), row.head, it)
+        end
+    end
+
+    type Node <: RowHead # hack
         left
+        right
         up
         down
         row
@@ -86,19 +106,20 @@ module DLX
             self = new()
             self.row = row
             self.col = col
+            self.left = self.right = self.up = self.down = self
             self
         end
     end
 
     function init(nrows, ncols, is_constraint)
-        root = RowHead()
-        for i in 1:ncols
-            push(root, ColumnHead())
+        root = Root()
+        for ci in 1:ncols
+            push(root, Column(ci))
         end
 
         for ri in 1:nrows
-            row = RowHead()
-            for (col, ci) in enumerate(Circular{Right}(root))
+            row = Row(ri)
+            for (col, ci) in enumerate(circular(Right, root))
                 if !is_constraint(ri, ci)
                     continue
                 end
@@ -108,30 +129,35 @@ module DLX
                 push(col, it)
             end
         end
+
+        root
     end
 
     function search(root)
         rows = {}
-        search(rows, 0)
+        search(root, rows, 0)
+        return
     end
 
-    function search(root, n, rows)
+    function search(root, rows, n)
         if root.right == root
-            return rows # throw?
+            println(rows)
+            return
         end
 
         col = choose_column(root)
         cover(col)
 
-        for row in Circular{Down}(col)
-            push(rows, row)
-            for j in Circular{Right}(row)
+        for row in circular(Down, col)
+            push(rows, row.row.ri)
+            for j in circular(Right, row)
                 cover(j.col)
             end
             search(root, rows, n + 1) # check for soln?
-            for j in Circular{Left}(row)
+            for j in circular(Left, row)
                 uncover(j.col)
             end
+            pop(rows)
         end
 
         uncover(col)
@@ -140,8 +166,8 @@ module DLX
     function cover(col)
         col.right.left = col.left
         col.left.right = col.right
-        for i in Circular{Down}(col)
-            for j in Circular{Right}(i)
+        for i in circular(Down, col)
+            for j in circular(Right, i)
                 j.down.up = j.up
                 j.up.down = j.down
                 j.col.size -= 1
@@ -150,8 +176,8 @@ module DLX
     end
 
     function uncover(col)
-        for i in Circular{Up}(col)
-            for j in Circular{Left}(i)
+        for i in circular(Up, col)
+            for j in circular(Left, i)
                 j.col.size += 1
                 j.down.up = j
                 j.up.down = j
@@ -165,6 +191,17 @@ module DLX
         root.right
     end
 
+    function test()
+        ex = [1 0 0 0 1
+              0 1 0 1 0
+              0 1 1 0 0
+              0 0 1 0 0
+              1 0 0 1 1]
+        f = (ri, ci) -> (ex[ri, ci] == 1)
+        root = init(size(ex)..., f)
+        search(root)
+        return
+    end
+
 end # module DLX
 
-import DLX.*
